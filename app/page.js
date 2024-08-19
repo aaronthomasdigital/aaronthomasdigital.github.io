5e84dc159012185f8779b9e7db3bfbced300548e
Aaron Robinson <theaaronrobinson@gmail.com>
2024-08-19 00:15:44 -0500

    updated logo and copy for coming soon

--- app/page.js
+++ app/page.js
@@ -4,7 +4,16 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image src={logoImg} alt="Aaron Thomas Digital" priority />
+      <Image
+        src={logoImg}
+        alt="Aaron Thomas Digital"
+        height={199}
+        width={399}
+        priority
+      />
+      <h1 className="text-black text-2xl font-mono font-black mt-8">
+        New Site Coming Soon.
+      </h1>
     </main>
   );
 }
